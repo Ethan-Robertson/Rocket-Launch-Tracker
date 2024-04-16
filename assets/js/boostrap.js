a81c78020a68e52f0393872280072cdc0c6dd77d
Patrick Gustafson <ptgt1999@gmail.com>
2024-04-15 20:30:29 -0400

    expanded on apis, css code was added, added bootstrap and coorosponding code to html

---
+++ assets/js/boostrap.js
@@ -0,0 +1,1 @@
+const requestUrl = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,17 @@ function getLaunchData() {
         console.log(data);
         for (let i = 0; i < 1; i++) {
             const launch = data.results[i];
-            console.log(launch);
+            console.log("launch", launch);
+            console.log("pad", launch.pad.location.name)
+            console.log("provider", launch.launch_service_provider.name)
+            console.log("launch_start", launch.window_start)
             console.log(launch.name, launch.image)
          const card = document.querySelector(".card")
             card.querySelector(".card-title").textContent = launch.name
-            card.querySelector(".card-img-top").src = launch.image
+            card.querySelector(".img-fluid").src = launch.image
+            card.querySelector(".provider").textContent = launch.launch_service_provider.name
+            card.querySelector(".pad").textContent = launch.pad.location.name
+            card.querySelector(".launch_start").textContent = launch.window_start
         }
     }) ;
 }
